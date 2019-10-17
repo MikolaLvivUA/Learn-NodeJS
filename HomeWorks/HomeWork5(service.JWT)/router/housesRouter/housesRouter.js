7c732de3d4555626595285d759490717cb333da4
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2019-10-17 16:20:05 +0300

    HomeWork5(little_code_styling)

--- HomeWorks/HomeWork5(service.JWT)/router/housesRouter/housesRouter.js
+++ HomeWorks/HomeWork5(service.JWT)/router/housesRouter/housesRouter.js
@@ -3,14 +3,26 @@ const router = require('express').Router();
 const {houseMiddleware, tokenMiddleware} = require('../../middleware');
 const {house} = require('../../controllers');
 
-router.post('/', tokenMiddleware.checkAccessTokenMiddleware, house.createNewHouse);
-router.get(`/:houseID`, houseMiddleware.presentHouseCheck, house.getHouseById);
-router.put('/:houseID', houseMiddleware.presentHouseCheck,
-    tokenMiddleware.checkAccessTokenMiddleware, house.updateHouse
+router.post('/',
+    tokenMiddleware.checkAccessTokenMiddleware,
+    house.createNewHouse
 );
 
-router.delete('/:houseID', houseMiddleware.presentHouseCheck,
-    tokenMiddleware.checkAccessTokenMiddleware, house.houseDelete
+router.get(`/:houseID`,
+    houseMiddleware.presentHouseCheck,
+    house.getHouseById
+);
+
+router.put('/:houseID',
+    houseMiddleware.presentHouseCheck,
+    tokenMiddleware.checkAccessTokenMiddleware,
+    house.updateHouse
+);
+
+router.delete('/:houseID',
+    houseMiddleware.presentHouseCheck,
+    tokenMiddleware.checkAccessTokenMiddleware,
+    house.houseDelete
 );
 
 module.exports = router;
--- HomeWorks/HomeWork5(service.JWT)/router/usersRouter/usersRouter.js
+++ HomeWorks/HomeWork5(service.JWT)/router/usersRouter/usersRouter.js
@@ -4,14 +4,21 @@ const {userMiddleware, tokenMiddleware} = require('../../middleware');
 const {user} = require('../../controllers');
 
 router.post('/', user.registerNewUser);
-router.get(`/:userID`, userMiddleware.presentUserCheck, user.getUserById);
-router.get(`/:userID/houses`, user.getUserWithHouses );
+router.get(`/:userID`,
+    userMiddleware.presentUserCheck,
+    user.getUserById
+);
+
+router.get(`/:userID/houses`,
+    user.getUserWithHouses
+);
 
 router.put('/:userID',
     userMiddleware.presentUserCheck,
     tokenMiddleware.checkAccessTokenMiddleware,
     user.updateUser
 );
+
 router.delete('/:userID',
     userMiddleware.presentUserCheck,
     tokenMiddleware.checkAccessTokenMiddleware,
